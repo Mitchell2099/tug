e1322fc5f08659e2c7d7901a0382b3380c03b4a8
Mitchell Morrison <mmorrison@aecon.com>
2019-04-05 13:03:24 -0400

    init_yo_spfx_extension

---
+++ src/extensions/tug/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ITugApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'TugApplicationCustomizerStrings' {
+  const strings: ITugApplicationCustomizerStrings;
+  export = strings;
+}
